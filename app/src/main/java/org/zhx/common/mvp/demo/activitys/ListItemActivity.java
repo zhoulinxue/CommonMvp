b54fa7763d935b052758013032e5beb0382074ac
zhouxue@lobochat.cn <zlx871026>
2020-07-14 16:57:59 +0800

    update  simple code

---
+++ app/src/main/java/org/zhx/common/mvp/demo/activitys/ListItemActivity.java
@@ -0,0 +1,76 @@
+package org.zhx.common.mvp.demo.activitys;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
+import org.zhx.common.mvp.demo.R;
+import org.zhx.common.mvp.demo.adapters.StringAdapter;
+import org.zhx.common.mvp.uikit.activitys.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ProjectName: CommonMvp
+ * @Package: org.zhx.common.mvp.demo.activitys
+ * @ClassName: ListItemActivity
+ * @Description:java类作用描述
+ * @Author: zhouxue
+ * @CreateDate: 2020/7/14 16:42
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2020/7/14 16:42
+ * @UpdateRemark: 更新说明
+ * @Version:1.0
+ */
+public class ListItemActivity extends BaseActivity {
+    @Override
+    public int initLayout() {
+        return R.layout.list_activity;
+    }
+
+    @Override
+    public void onCreatView() {
+        RecyclerView recyclerView = findViewById(R.id.list_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        List<String> datas = new ArrayList<>();
+        datas.add("activity 中 使用效果");
+        datas.add("fragment 中使用效果");
+        StringAdapter adapter = new StringAdapter(datas);
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(ListItemActivity.this, MainActivity.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(ListItemActivity.this, FragmentActivity.class));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onLoadDataFromSavedInstanceState(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onLoadContent() {
+
+    }
+}
